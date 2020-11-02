2dd9eede0e2b2869cd16991f3c3dc2dfc64bb356
brookeperkins <brookemmperkins@gmail.com>
2020-11-01 18:42:21 -0800

    model and post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,28 @@
-const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const fs = require('fs');
 
 describe('bjork-api-be routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a new bjork outfit entry', () => {
+    return request(app)
+      .post('/api/outfits')
+      .send({
+        img: 'bjork.jpg',
+        year: 1995,
+        quote: 'some delightfully weird bjork quote'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          img: 'bjork.jpg',
+          year: 1995,
+          quote: 'some delightfully weird bjork quote'
+        });
+      });
   });
 });
